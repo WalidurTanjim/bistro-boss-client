65acf2194474677a910cfdfddc5f5f48da97c1e9
WalidurTanjim <mohammadtanjim91@gmail.com>
2024-12-29 23:39:47 +0600

    bg image added using tailwind with pass image as params

--- src/components/CoverSection/CoverSection.jsx
+++ src/components/CoverSection/CoverSection.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import './CoverSection.css';
 
-const CoverSection = ({ title, sub_title}) => {
+const CoverSection = ({ title, sub_title, image}) => {
     return (
-        <section className='coverSection relative w-full h-[500px] md:h-[400px] flex items-center justify-center mt-10'>
+        <section className={`coverSection relative w-full h-[500px] md:h-[400px] flex items-center justify-center mt-10`} style={{ backgroundImage: `url(${image})`, backgroundRepeat: 'no-repeat', backgroundSize: "100% 100%"}}>
             <div className="absolute mx-auto px-8 w-full md:w-[600px] lg:w-[750px]">
                 <div className='bg-black/40 py-10 px-6 text-center'>
                     <h1 className='text-xl md:text-2xl lg:text-3xl font-medium text-white mb-2'>{title}</h1>
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -7,13 +7,14 @@ import CallUs from '../CallUs/CallUs';
 import ChefRecommends from '../ChefRecommends/ChefRecommends';
 import FeaturedBanner from '../FeaturedBanner/FeaturedBanner';
 import Testimonials from '../Testimonials/Testimonials';
+import chefImage from '../../../assets/home/chef-service.jpg';
 
 const Home = () => {
     return (
         <section className='home'>
             <Banner />
             <Category />
-            <CoverSection title="Bistro Boss" sub_title="Bistro Boss is a modern restaurant website showcasing a variety of delicious cuisines with an elegant design. It features a user-friendly interface for browsing menus, booking tables, and exploring special offers, ensuring a delightful dining experience online." />
+            <CoverSection title="Bistro Boss" sub_title="Bistro Boss is a modern restaurant website showcasing a variety of delicious cuisines with an elegant design. It features a user-friendly interface for browsing menus, booking tables, and exploring special offers, ensuring a delightful dining experience online." image={chefImage} />
             <FeaturedMenu />
             <CallUs />
             <ChefRecommends />
