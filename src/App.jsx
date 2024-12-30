d4fc69bcb4f4f75a61df1118f69c562949f1adf5
WalidurTanjim <mohammadtanjim91@gmail.com>
2024-12-31 01:35:41 +0600

    Our Location add to Contact Us page

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import MainLayout from './layout/MainLayout'
 import Home from './pages/Home/Home/Home'
 import OurMenus from './pages/OurMenus/OurMenus/OurMenus'
 import OurShop from './pages/OurShop/OurShop/OurShop'
+import ContactUs from './pages/ContactUs/ContactUs'
 
 function App() {
   const routers = createBrowserRouter([
     {path: '/', errorElement: <ErrorPage />, element: <MainLayout />, children: [
       {path: '/', element: <Home />},
       {path: '/our-menu', element: <OurMenus />},
-      {path: '/our-shop', element: <OurShop />}
+      {path: '/our-shop', element: <OurShop />},
+      {path: '/contact-us', element: <ContactUs />}
     ]}
   ])
 
---
+++ src/pages/ContactUs/ContactUs.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import CoverSection from '../../components/CoverSection/CoverSection';
+import bannerImage from '../../assets/contact/banner.jpg';
+import { PhoneIcon, MapPinIcon, ClockIcon } from '@heroicons/react/24/solid'
+import SectionTitle from '../../components/SectionTitle/SectionTitle';
+
+const ContactUs = () => {
+    return (
+        <section className="contact-us">
+            <CoverSection title="Contact Us" sub_title="WOULD YOU LIKE TO TRY A DISH" image={bannerImage} />
+
+            {/* our-location div starts */}
+            <div className="our-location container mx-auto px-6 py-10">
+                <SectionTitle sub_title="Visit Us" title="Our Location" />
+
+                <div className='grid gap-5 grid-cols-1 md:grid-cols-3 '>
+                    <div className="border">
+                        <div className='w-full py-3 flex items-center justify-center bg-yellow-500'>
+                            <PhoneIcon className="size-7 text-white" />
+                        </div>
+
+                        <div className='p-5 pt-0 h-[150px]'>
+                            <div className="bg-gray-100 text-center h-full flex items-center justify-center">
+                                <div>
+                                    <h1 className='text-xl md:text-2xl font-medium uppercase pb-1'>Location</h1>
+                                    <p>+880 1752 145978</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="border">
+                        <div className='w-full py-3 flex items-center justify-center bg-yellow-500'>
+                            <MapPinIcon className="size-7 text-white" />
+                        </div>
+
+                        <div className='p-5 pt-0 h-[150px]'>
+                            <div className="bg-gray-100 text-center h-full flex items-center justify-center">
+                                <div>
+                                    <h1 className='text-xl md:text-2xl font-medium uppercase pb-1'>Address</h1>
+                                    <p>+880 1752 145978</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="border">
+                        <div className='w-full py-3 flex items-center justify-center bg-yellow-500'>
+                            <ClockIcon className="size-7 text-white" />
+                        </div>
+
+                        <div className='p-5 pt-0 h-[150px]'>
+                            <div className="bg-gray-100 text-center h-full flex items-center justify-center">
+                                <div>
+                                    <h1 className='text-xl md:text-2xl font-medium uppercase pb-1'>Working Hours</h1>
+                                    <p>+880 1752 145978</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default ContactUs;
