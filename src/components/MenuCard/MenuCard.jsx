5b9bea568bdaa4483d89519225c6426c7fab31df
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-01-02 15:02:08 +0600

    MenuCard created & tabIndex added to display tabname in the url

---
+++ src/components/MenuCard/MenuCard.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Spinner from '../Spinner/Spinner';
+
+const MenuCard = ({ meals, isPending, isError, error }) => {
+    return (
+        <>
+            <div>
+                {
+                    isPending ? (
+                        <Spinner />
+                    ) : isError ? (
+                        <div className='w-full py-14 flex items-center justify-center'>
+                            <h1 className='text-xl md:text-2xl lg:text-3xl text-red-500 font-medium'>{error?.message}</h1>
+                        </div>
+                    ) :
+                        <div className='grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3'>
+                            {
+                                meals?.map(item => {
+                                    const { _id, image, name, price, recipe } = item;
+
+                                    return (
+                                        <div key={_id} className="card bg-base-100 w-full border shadow-md hover:shadow-lg">
+                                            <figure className="px-7 pt-7">
+                                                <img
+                                                    src={image}
+                                                    alt=""
+                                                    className="rounded-lg" />
+                                            </figure>
+                                            <div className="card-body items-center text-center">
+                                                <h2 className="card-title">{name}</h2>
+                                                <p>{recipe.length > 45 ? recipe.slice(0, 45)+'...' : recipe}</p>
+                                                <div className="card-actions">
+                                                    <button className="btn btn-primary">Add To Card</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                }
+            </div>
+        </>
+    );
+};
+
+export default MenuCard;
--- src/pages/OurShop/OurShop/OurShop.jsx
+++ src/pages/OurShop/OurShop/OurShop.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import bannerImage from '../../../assets/shop/banner2.jpg';
 import CoverSection from '../../../components/CoverSection/CoverSection';
 
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import useMenu from '../../../hooks/useMenu';
-import CategoryMenuWithButton from '../../../components/CategoryMenuWithButton/CategoryMenuWithButton';
+import MenuCard from '../../../components/MenuCard/MenuCard';
 // import 'react-tabs/style/react-tabs.css';
 
 
 const OurShop = () => {
+    const [tabIndex, setTabIndex] = useState(0);
+    console.log('Tabindex:', tabIndex)
     const [ menu, isPending, isError, error, refetch ] = useMenu();
     const popularMenu = menu?.filter(menuItem => menuItem?.category.toLowerCase() === 'popular');
     const saladMenu = menu?.filter(menuItem => menuItem?.category.toLowerCase() === 'salad');
@@ -22,7 +24,7 @@ const OurShop = () => {
             <CoverSection title="Our Shop" sub_title="WOULD YOU LIKE TO TRY A DISH" image={bannerImage} />
 
             <div className='container mx-auto px-6 py-14'>
-                <Tabs>
+                <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
                     <TabList className="flex items-center justify-center gap-x-3 w-full uppercase overflow-auto">
                         <Tab className="cursor-pointer px-3 py-1 rounded-md hover:bg-gray-100 font-medium">Salad</Tab>
                         <Tab className="cursor-pointer px-3 py-1 rounded-md hover:bg-gray-100 font-medium">Pizza</Tab>
@@ -32,23 +34,23 @@ const OurShop = () => {
                     </TabList>
 
                     <TabPanel className="pt-10">
-                        <CategoryMenuWithButton meals={saladMenu} isPending={isPending} isError={isError} error={error} />
+                        <MenuCard meals={saladMenu} isPending={isPending} isError={isError} error={error} />
                     </TabPanel>
                     
                     <TabPanel>
-                        <CategoryMenuWithButton meals={pizzaMenu} isPending={isPending} isError={isError} error={error} />
+                        <MenuCard meals={pizzaMenu} isPending={isPending} isError={isError} error={error} />
                     </TabPanel>
 
                     <TabPanel>
-                        <CategoryMenuWithButton meals={soupMenu} isPending={isPending} isError={isError} error={error} />
+                        <MenuCard meals={soupMenu} isPending={isPending} isError={isError} error={error} />
                     </TabPanel>
 
                     <TabPanel>
-                        <CategoryMenuWithButton meals={dessertMenu} isPending={isPending} isError={isError} error={error} />
+                        <MenuCard meals={dessertMenu} isPending={isPending} isError={isError} error={error} />
                     </TabPanel>
 
                     <TabPanel>
-                        <CategoryMenuWithButton meals={drinksMenu} isPending={isPending} isError={isError} error={error} />
+                        <MenuCard meals={drinksMenu} isPending={isPending} isError={isError} error={error} />
                     </TabPanel>
                 </Tabs>
             </div>
