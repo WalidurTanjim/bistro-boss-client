e2c3e16ab8df11f2265d4173baa3dd48f3badd24
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-01-06 15:58:05 +0600

    display table body row based on add to cart length

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,12 +1,14 @@
+import useAuth from './useAuth';
 import useAxiosSecure from './useAxiosSecure';
 import { useQuery } from '@tanstack/react-query';
 
 const useCart = () => {
+    const { user } = useAuth();
     const axiosSecure = useAxiosSecure();
     const { data: cart = [], isPending, isError, error, refetch } = useQuery({
-        queryKey: ['cart'],
+        queryKey: ['cart', user?.email],
         queryFn: async() => {
-            const res = await axiosSecure.get('/carts');
+            const res = await axiosSecure.get(`/carts?email=${user?.email}`);
             const data = await res?.data;
             if(data){
                 return data;
